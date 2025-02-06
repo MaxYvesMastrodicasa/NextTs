a35d82a1d95c0f5cfe3d03919506ff64a1ebad35
MaxYvesMastrodicasa <maxyves.mastrodicasa@gmail.com>
2025-02-06 09:06:29 +0100

    Init : arborescence

--- musilearn/tailwind.config.ts
+++ musilearn/tailwind.config.ts
@@ -16,3 +16,10 @@ export default {
   },
   plugins: [],
 } satisfies Config;
+
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
+
